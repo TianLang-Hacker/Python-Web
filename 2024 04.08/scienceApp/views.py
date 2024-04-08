ab48f9bb0f9fc8b0e865817954d2b632cd6505eb
TianLang-Hacker <3071571983@qq.com>
2024-04-08 11:05:31 +0800

    6

---
+++ 2024 04.08/scienceApp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+def science(request):  # 科研基地
+    html='<html><body>科研基地</body></html>'
+    return HttpResponse(html)
